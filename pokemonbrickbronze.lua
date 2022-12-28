4f9be5095234067b2d3849fa7009845ff9394bdb
YemenDoesCoding <119768554+YemenDoesCoding@users.noreply.github.com>
2022-12-28 14:58:30 -0800

    Update pokemonbrickbronze.lua

--- pokemonbrickbronze.lua
+++ pokemonbrickbronze.lua
@@ -234,19 +234,11 @@ Movement:AddSlider({
 	Increment = 1,
 	ValueName = "",
 	Callback = function(Value)
-		local walkSpeed = 16
-            local gmt = getrawmetatable(game)
-            setreadonly(gmt, false)
-            local oldindex = gmt.__index
-gmt.__index = newcclosure(function(self,b)
-if b == "WalkSpeed" then
-return 16
-end
-return oldindex(self,b)
-end)
 game:GetService("Players").LocalPlayer.Character.Humanoid.WalkSpeed = walkSpeed
+
 	end    
 })
+
 
 
 OrionLib:Innit()
